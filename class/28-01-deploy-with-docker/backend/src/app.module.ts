97763d3d80cf73207870b66362ab8e338edbb5e4
YoungDogg <leejonghwo@gmail.com>
2022-02-28 11:10:59 +0900

    Deploy

--- class/28-01-deploy-with-docker/backend/src/app.module.ts
+++ class/28-01-deploy-with-docker/backend/src/app.module.ts
@@ -25,7 +25,7 @@ import { ConfigModule } from '@nestjs/config';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'my_', // gcp ip 주소로다가
+      host: 'my_database', // gcp ip 주소로다가
       port: 3306,
       username: 'root',
       password: 'root',
